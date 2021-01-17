233bb36e5c14e3b37a5eacde5e61b3e9dfe0203d
shivanshu-pattnaik <74895692+ShivanshuPattnaik@users.noreply.github.com>
2021-01-17 19:20:33 +0530

    Add files via upload

---
+++ greenbox.js
@@ -0,0 +1,37 @@
+class GreenBox{
+
+    constructor(x, y){
+
+        var options = {
+
+            isStatic : false,
+            'restitution' : 0,
+            'friction' : 5,
+            'density' : 0.1
+
+        }
+        
+        this.body = Bodies.rectangle(x, y, 25, 50, options);
+        this.width = 25;
+        this.height = 50;
+
+        World.add(world, this.body);
+
+    }
+
+    display(){
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill("green");
+        rect(0, 0, this.width, this.height);
+        pop();
+
+    }
+
+}
